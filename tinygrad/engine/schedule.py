8bbcccead993e93b0a3e153d0eb1fc7d2cca5cb4
qazal <qazal.software@gmail.com>
2024-12-18 00:45:58 +0200

    is_bufferized

--- tinygrad/engine/schedule.py
+++ tinygrad/engine/schedule.py
@@ -53,7 +53,7 @@ def to_uop(buf:UOp, ctx:ScheduleContext, cache:Dict[UOp, UOp]) -> UOp:
   # shapeless op is passthrough
   # realized is passthrough
   # CONST, BIND are passthrough
-  if buf.st is None or buf.base.is_realized or (buf.op is Ops.VIEW and buf.src[1].op in {Ops.CONST, Ops.BIND}): return buf
+  if buf.st is None or buf.base.is_realized or buf.base.is_unrealized_const(): return buf
   # view is passthrough
   if buf is not buf.base:
     cache[buf] = ret = to_uop(buf.base, ctx, cache).view(buf.st)
@@ -212,7 +212,9 @@ if getenv("RUN_PROCESS_REPLAY"):
 
 # **** Schedule grouping
 
-def is_scheduled(u:UOp) -> bool: return u.op is Ops.VIEW and len(u.src) == 2 and u.src[1].op not in {Ops.CONST, Ops.BIND}
+# TODO: rename this to is_bufferized
+# NOTE: this needs to exist to map tensor uops into bufferized VIEWs
+def is_scheduled(u:UOp) -> bool: return u.op is Ops.VIEW and len(u.src) == 2 and u.src[0].op is Ops.BUFFER
 def uval(u:UOp) -> UOp:
   assert is_scheduled(u), f"must be a scheduled op {u}"
   return r.src[0] if (r:=u.src[1]).op is Ops.CONTIGUOUS and not (r.src[0].base.op is Ops.VIEW and len(r.src[0].base.src) == 2) else r
